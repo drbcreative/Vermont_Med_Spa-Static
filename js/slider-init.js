773f46ad321b637c2a9e3e1dfa342a61ff71b0a6
Danny <holler@dannyburns.me>
2019-03-25 23:02:30 -0700

    almost done

--- js/slider-init.js
+++ js/slider-init.js
@@ -1,48 +1,19 @@
 $(document).ready(function() {
+  $(".testimonial-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    autoplay: true,
+    arrows: false
+  });
   $(".hero").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: !0,
-    dots: false,
-    // responsive: [
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: !0,
-    //       dots: !1
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       infinite: !0,
-    //       dots: !1
-    //     }
-    //   }
-    // ],
-    // nextArrow: $(".slide-right-2"),
-    // prevArrow: $(".slide-left-2"),
-    autoplay: true
+    dots: !1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 5000
   });
-  // $(".testimonial-slider").slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   infinite: !0,
-  //   dots: true,
-  //   autoplay: true,
-  //   arrows: false
-  // });
-  // $(".hero-slider").slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   infinite: !0,
-  //   dots: !1,
-  //   autoplay: true,
-  //   arrows: false,
-  //   autoplaySpeed: 5000
-  // });
 });
